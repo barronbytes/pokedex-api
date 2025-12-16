038285e6645be08d34ddd369e491fa58bda0737f
barronbytes <jonathan.m.barron@gmail.com>
2025-12-15 22:33:59 -0500

    Created startREPL method

--- src/repl.ts
+++ src/repl.ts
@@ -7,17 +7,21 @@ interface Terminal {
 }
 
 
-export function cleanInput(input: string): Array<string> {
+const terminal: Terminal = {
+    input: process.stdin,
+    output: process.stdout,
+    prompt: () => {
+        terminal.output.write("> ");
+    }
+}
+
+
+function cleanInput(input: string): Array<string> {
     const inputSplit = input.split(" ");
     return inputSplit.filter(n => n.length > 0);
 }
 
-export const startREPL: Terminal = {
-    input: process.stdin,
-    output: process.stdout,
-    prompt: () => {
-        startREPL.output.write("> ");
-    }
-}
 
-startREPL.prompt()
+export function startREPL() {
+    terminal.prompt()
+}
