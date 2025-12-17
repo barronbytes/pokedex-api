6e7c302ca3bc1850dd008bc0795418a35600df3a
barronbytes <jonathan.m.barron@gmail.com>
2025-12-16 19:52:12 -0500

    readline implementation

--- src/repl.ts
+++ src/repl.ts
@@ -1,28 +1,28 @@
-import { Readable, Writable } from "node:stream";
-
-interface Terminal {
-    input: Readable,
-    output: Writable,
-    prompt: () => void;
-    on: (event: "line", callback: (userInput: string) => void) => void;
-}
+import readline from "node:readline";
 
 
-const terminal: Terminal = {
-    input: process.stdin,
-    output: process.stdout,
-    prompt: () => {
-        terminal.output.write("Pokedex > ");
-    }
-}
+const repl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex > "
+});
 
 
-function cleanInput(input: string): Array<string> {
+export function cleanInput(input: string): Array<string> {
     const inputSplit = input.split(" ");
     return inputSplit.filter(n => n.length > 0);
 }
 
 
 export function startREPL() {
-    terminal.prompt()
+    repl.prompt();
+    repl.on("line", (line) => {
+        const inputs = cleanInput(line);
+        if(!inputs.length) {
+            repl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${inputs[0].toLowerCase()}`);
+        repl.prompt();
+    });
 }
